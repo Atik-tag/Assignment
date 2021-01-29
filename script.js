7bf02f9b2bdcf1443d044bed9c7401af1b830814
Atik-tag <34893718+Atik-tag@users.noreply.github.com>
2021-01-29 23:27:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+<script>
+alert("Hi Team");
+<p> Sorting Numbers> </p>
+let numbers = [0, 1 , 2, 3, 10, 20, 30 ];
+numbers.sort( function( a , b){
+    if(a > b) return 1;
+    if(a < b) return -1;
+    return 0;
+});
+
+console.log(numbers);
+</script>
